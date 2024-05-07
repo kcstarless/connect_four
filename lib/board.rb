0f4bbc54faafa5471c9300e97a85eaac1d2d7e08
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-05-07 11:53:06 +1000

    Added vertical & horizontal line check method

--- lib/board.rb
+++ lib/board.rb
@@ -18,14 +18,68 @@ class Board
   end
 
   def place_marker(col, player)
-    h = BOARD_HEIGHT - 1
-    while h >= 0
-      if board[h][col] == "."
-        board[h][col] = player.mark
+    row = BOARD_HEIGHT - 1
+    while row >= 0
+      if board[row][col] == "."
+        board[row][col] = player.mark
+        check_line(row, col, player)
         break
       end
-      h -= 1
+      row -= 1
     end
     display_board
+  end
+
+  private
+
+  # Check if line is formed
+  def check_line(row, col, player)
+    line_formed = false
+    line_formed = true if horizontal(row, col, player)
+    line_formed = true if vertical(row, col, player)
+    line_formed = true if diagonal(row, col, player)
+    line_formed
+  end
+
+  # Horizontal line check
+  def horizontal(row, col, player)
+    right = h_right(row, col + 1, player)
+    left = h_left(row, col - 1, player)
+    puts "Player horizontal #{player.mark}: #{right + left + 1}"
+    true if right + left >= 4
+  end
+
+  def h_right(row, col, player, count = 0)
+    return count if board[row][col] != player.mark
+
+    count += 1 if board[row][col] == player.mark || col < 7
+    count = h_right(row, col + 1, player, count)
+  end
+
+  def h_left(row, col, player, count = 0)
+    return count if board[row][col] != player.mark || col < 0
+
+    count += 1 if board[row][col] == player.mark
+    count = h_left(row, col - 1, player, count)
+  end
+  
+  # Vertical Line Check
+  def vertical(row, col, player)
+    down = v_down(row + 1, col, player)
+    puts "Player vertical #{player.mark}: #{down + 1}"
+    true if down >= 4
+  end
+
+  def v_down(row, col, player, count = 0)
+    return count if row > 5
+    return count if board[row][col] != player.mark
+
+    count += 1 if board[row][col] == player.mark
+    count = v_down(row + 1, col, player, count)
+  end
+
+  # Diagonal line check
+  def diagonal(row, col, player)
+    left_to_right = d_left_to_right(row, col, player)
   end
 end
